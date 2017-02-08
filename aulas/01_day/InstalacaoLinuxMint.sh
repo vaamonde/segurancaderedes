3082aeb30d0255212244d78ce5ef0198f86646c4
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-02-08 13:44:52 -0200

    Create InstalacaoLinuxMint.sh

---
+++ aulas/01_day/InstalacaoLinuxMint.sh
@@ -0,0 +1,28 @@
+Instalação do GNU/Linux Mint 18.1 Cinnamon x64 em Notebook Itautec
+
+Download da ISO: https://www.linuxmint.com/edition.php?id=226
+
+Download do RUFUS: https://rufus.akeo.ie/?locale=pt_BR
+
+Apostila GNU/Linux Mint: https://www.linuxmint.com/documentation/user-guide/Cinnamon/portuguese_brazil_18.0.pdf
+
+Vídeo Aulas do GNU/Linux Mint: https://www.youtube.com/playlist?list=PLozhsZB1lLUNcpyrspEOqpQHMQqoe1wDJ
+
+Vídeo Aulas do GNU/Linux Ubuntu: https://www.youtube.com/playlist?list=PLozhsZB1lLUMHaZmvczDWugUv9ldzX37u
+
+Download do Oracle VirtualBOX: https://www.virtualbox.org/wiki/Linux_Downloads
+
+Vídeo Aulas do Oracle VirtualBOX e Hyper-V: http://professorramos.com/index.php/categoria/aulas/sistema-operacional/virtualizacao/
+
+Procedimentos que serão utilizados na aula:
+
+01. Criação do Pen-Driver utilizando o RUFUS no Windows;
+02. Configuração da BIOS dos Notebooks;
+03. Inicialização da Instalação via Pen-Driver;
+04. Conhecendo o Boot Load do GNU/Linux Mint;
+05. Rodando o GNU/Linux Mint via Live;
+06. Instalando o GNU/Linux Mint;
+07. Atualizando o GNU/Linux Mint;
+08. Download do Oracle VirtualBOX;
+09. Instalação do Oracle VirtualBOX;
+10. Criação das Máquinas Virtuais.
