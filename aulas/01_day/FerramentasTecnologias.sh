1975d7d0d038ab3c779c6afc6952c16e81fce92f
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-02-08 13:59:30 -0200

    Create FerramentasTecnologias.sh

---
+++ aulas/01_day/FerramentasTecnologias.sh
@@ -0,0 +1,48 @@
+Soluções que serão utilizadas no curso
+
+Inventário de Rede:	
+Sistema Operacional: Ubuntu Server 16.04 LTS x64 
+Download: http://releases.ubuntu.com/16.04/
+
+Serviços de Rede: LAMP Server
+Apache2: https://httpd.apache.org/
+Oracle MySQL: https://www.mysql.com/
+PHP: https://secure.php.net/
+PERL: https://www.perl.org/
+PYTHON: https://www.python.org/
+
+Software de Inventário: OCS Inventory;
+Download: http://www.ocsinventory-ng.org/en/
+
+Sistemas Operacionais utilizados no Curso
+Microsoft Windows: https://www.microsoft.com/pt-br/windows/
+GNU/Linux Ubuntu: https://www.ubuntu.com/
+GNU/Linux Mint: https://www.linuxmint.com/
+Android x86: http://www.android-x86.org/
+Genymotion: https://www.genymotion.com/
+
+Sistema de Backup e Restore
+Clonagem de Hard Disk com Clonezilla: http://clonezilla.org/
+Backup com Bareos Backup: https://www.bareos.org/en/
+
+Sistema de AAA (Autenticação, Autorização e Contabilização)
+Sistema Operacional: Ubuntu Server 16.04 LTS x64 
+Active Directory Domain Service: SAMBA-4: https://www.samba.org/
+Administração do Active Directory RSAT: https://www.microsoft.com/pt-br/download/details.aspx?id=7887
+
+Sistema de Firewall, Proxy e VPN
+UTM Firewall Endian: http://www.endian.com/
+UTM Firewall pfSense: https://pfsense.org/
+
+Sistema de Geração de Certificação CA (Autoridade Certificadora)
+Software: OpenSSL: https://www.openssl.org/
+Criação de CA: SSL Enterprise Key Management Infrastructure (OASSIS)
+https://www.vivaolinux.com.br/artigo/Criando-Autoridades-Certificadores-e-Certificados-de-Testes-no-Linux?pagina=6
+
+Sistema de Proteção contra software Malicíosos
+Antivírus Kaspersky: https://www.kaspersky.com.br/
+Aitivírus Shopos: https://www.sophos.com/en-us.aspx
+http://www.toptenreviews.com/software/security/best-antivirus-software/
+
+Sistema Operacional de Teste de Intrusão
+Pentest: Kali Linux: https://www.kali.org/
