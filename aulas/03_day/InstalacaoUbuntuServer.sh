13e608c5cba3f3074a748b5a4ccafdc71ebb53cd
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-02-13 12:05:54 -0200

    Update InstalacaoUbuntuServer.sh

--- aulas/03_day/InstalacaoUbuntuServer.sh
+++ aulas/03_day/InstalacaoUbuntuServer.sh
@@ -1,1 +1,14 @@
-Teste
+Instalação e configuração do GNU/Linux Ubuntu Server
+
+Informações sobre RAID: https://pt.wikipedia.org/wiki/RAID
+
+Informações sobre LVM: https://en.wikipedia.org/wiki/Logical_Volume_Manager_(Linux)
+
+Informações sobre Sistema de Arquivos GNU/Linux: https://pt.wikipedia.org/wiki/Sistema_de_ficheiros
+
+01. Criação da Máquina Virtual no Virtual BOX;
+02. Personalização da Máquina Virtual;
+03. Criação de Hard Disk para configuração de RAID;
+04. Instalação do GNU/Linux Ubuntu Server 16.04.x LTS;
+05. Atualização do Sistema Operacional;
+06. Configurações pós instalação.
